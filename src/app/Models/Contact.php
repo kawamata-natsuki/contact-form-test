d125e5e75ed5048d2e0545f1aac2ddf7374a243b
kawamata natsuki <t.natsuki020817@gmail.com>
2025-03-06 02:20:29 +0900

    Add Category and Content models with fillable attributes

---
+++ src/app/Models/Contact.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Contact extends Model
+{
+    use HasFactory;
+
+    # マスアサイメントの設定
+    protected $fillable = [
+        'category-id',
+        'first_name',
+        'last_name',
+        'gender',
+        'email',
+        'tel',
+        'address',
+        'building',
+        'detail'
+    ];
+
+    # リレーションの定義
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
