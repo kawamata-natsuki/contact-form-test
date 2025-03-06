eec2bb0d600c658cb3e3804dd070a20581551989
kawamata natsuki <t.natsuki020817@gmail.com>
2025-03-06 13:52:40 +0900

    Configure Fortify authentication settings

---
+++ src/app/Providers/FortifyServiceProvider.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Providers;
+
+use App\Actions\Fortify\CreateNewUser;
+use App\Actions\Fortify\ResetUserPassword;
+use App\Actions\Fortify\UpdateUserPassword;
+use App\Actions\Fortify\UpdateUserProfileInformation;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
+use Laravel\Fortify\Fortify;
+
+class FortifyServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    public function boot(): void
+    {
+        # 新規ユーザー登録処理
+        Fortify::createUsersUsing(CreateNewUser::class);
+
+        # 登録ページのビュー指定
+        Fortify::registerView(function () {
+            return view('register');
+        });
+
+        # ログインページのビュー指定
+        Fortify::loginView(function () {
+            return view('login');
+        });
+
+        # ログイン回数の制限を設定
+        RateLimiter::for('login', function (Request $request) {
+            $email = (string) $request->email;
+            return Limit::perMinute(10)->by($email . $request->ip());
+        });
+    }
+}
