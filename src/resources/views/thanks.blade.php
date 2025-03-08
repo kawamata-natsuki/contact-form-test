aa3c6cfe954a72494d4698efa89d88a5959682fe
kawamata natsuki <t.natsuki020817@gmail.com>
2025-03-08 12:48:13 +0900

    Create thanks.blade.php

--- src/resources/views/thanks.blade.php
+++ src/resources/views/thanks.blade.php
@@ -5,14 +5,27 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>お問い合わせフォーム</title>
+  <link rel="stylesheet" href="{{ asset('sanitize.css')}}">
+  <link rel="stylesheet" href="{{ asset('common.css')}}">
+  <link rel="stylesheet" href="{{ asset('thanks.css') }}">
 </head>
 
 <body>
 
-  <h1>thanks.blade.php</h1>
-  <h2>サンクスページ</h2>
+  <main>
 
-  <a href="/">HOME</a>
+    <div class="thanks-container">
+      <div class="thanks__message">
+        <h2>お問い合わせありがとうございました</h2>
+      </div>
+      <div class="home__button">
+        <form action="/" method="get">
+          <button class="home__button-subimit" type="submit">HOME</button>
+        </form>
+      </div>
+    </div>
+
+  </main>
 
 </body>
 
