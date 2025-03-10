a79efa1181de6dccceacbfd3681d6c566b4d72ba
kawamata natsuki <t.natsuki020817@gmail.com>
2025-03-10 16:19:25 +0900

    Add Css style in common.css

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -6,9 +6,12 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>お問い合わせフォーム</title>
 
+  <!-- フォントの読み込み -->
+  <link href="https://fonts.googleapis.com/css2?family=Inika&display=swap" rel="stylesheet">
+  <!-- モーダルウィンドウの読み込み -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="{{ asset('sanitize.css')}}">
-  <link rel="stylesheet" href="{{ asset('common.css')}}">
+  <link rel="stylesheet" href="{{ asset('css/sanitize.css')}}">
+  <link rel="stylesheet" href="{{ asset('css/common.css')}}">
   @yield('css')
 
 </head>
@@ -17,18 +20,14 @@
 
   <header>
     <div class="header__inner">
-
       <div class="header__logo">
-        <h1>FashionablyLate</h1>
+        <h1 class="header__logo-title">FashionablyLate</h1>
       </div>
       <div class="header-nav">
         <div class="header-nav__item">
           @yield('link')
         </div>
       </div>
-
-    </div>
-
     </div>
   </header>
 
